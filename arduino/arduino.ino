3ed61a5f7fee47a926719f7aac29ba9cdce687b5
Roman3349 <ondracek.roman@centrum.cz>
2016-02-10 21:36:35 +0100

    Added implementation of setcolor packet

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -38,7 +38,10 @@ void MyIqrfRxHandler();
 void MyIqrfTxHandler(UINT8 txPktId, UINT8 txPktResult);
 void AppTimerHandler();
 void cb_timer1ms();
-
+// Set adress to 00.00.00.00
+uint8_t my_address[4] = {0x00, 0x00, 0x00, 0x00};
+// Set version of protocol
+uint8_t ver = 0x00;
 
 /**
  * Global variables
@@ -104,30 +107,33 @@ void MyIqrfRxHandler(void) {
   Serial.print("IQRF receive done: ");
   Serial.write(app_vars.myIqrfRxBuf, IQRF_GetRxDataSize());
   Serial.println();
-  char red_buffer[3];
-  red_buffer[0] = app_vars.myIqrfRxBuf[0];
-  red_buffer[1] = app_vars.myIqrfRxBuf[1];
-  red_buffer[2] = app_vars.myIqrfRxBuf[2];
-  int red_value = atoi(red_buffer);
-  Serial.println("Red: " + red_value);
-  char green_buffer[3];
-  green_buffer[0] = app_vars.myIqrfRxBuf[3];
-  green_buffer[1] = app_vars.myIqrfRxBuf[4];
-  green_buffer[2] = app_vars.myIqrfRxBuf[5];
-  int green_value = atoi(green_buffer);
-  Serial.println("Green: " + green_value);
-  char blue_buffer[3];
-  blue_buffer[0] = app_vars.myIqrfRxBuf[6];
-  blue_buffer[1] = app_vars.myIqrfRxBuf[7];
-  blue_buffer[2] = app_vars.myIqrfRxBuf[8];
-  int blue_value = atoi(blue_buffer);
-  Serial.println("Blue: " + blue_value);
-  int alpha_value = app_vars.myIqrfRxBuf[9];
-  Serial.println("Aplha:" + alpha_value);
-  if (alpha_value == 0) {
-    analogWrite(red_led, red_value);
-    analogWrite(green_led, green_value);
-    analogWrite(blue_led, blue_value);
+  uint8_t prot_version = app_vars.myIqrfRxBuf[0];
+  Serial.println("Protocol version: " + prot_version);
+  if (prot_version == 0x00) {
+    uint8_t prot_type = app_vars.myIqrfRxBuf[1];
+    Serial.println("Protocol type: " + prot_type);
+    if (prot_type == 0x01) {
+      uint8_t address[4] = {app_vars.myIqrfRxBuf[2], app_vars.myIqrfRxBuf[3], app_vars.myIqrfRxBuf[4], app_vars.myIqrfRxBuf[5]};
+      Serial.print("Address: " + address[0]);
+      Serial.print(address[1] + ".");
+      Serial.print(address[2] + ".");
+      Serial.println(address[3]);
+      if (address == my_address) {
+        uint8_t red_value = app_vars.myIqrfRxBuf[6];
+        Serial.println("Red: " + red_value);
+        uint8_t green_value = app_vars.myIqrfRxBuf[7];
+        Serial.println("Green: " + green_value);
+        uint8_t blue_value = app_vars.myIqrfRxBuf[8];
+        Serial.println("Blue: " + blue_value);
+        uint8_t alpha_value = app_vars.myIqrfRxBuf[9];
+        Serial.println("Aplha:" + alpha_value);
+        if (alpha_value == 0x00) {
+          analogWrite(red_led, red_value);
+          analogWrite(green_led, green_value);
+          analogWrite(blue_led, blue_value);
+        }
+      }
+    }
   }
 }
 
