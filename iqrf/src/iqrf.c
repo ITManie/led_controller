e84b1dc233d9846953f404fb61538aad335e7be2
Roman3349 <ondracek.roman@centrum.cz>
2016-03-30 20:57:55 +0200

    [IQRF] Update sotfware for IQRF module

--- iqrf/src/iqrf.c
+++ iqrf/src/iqrf.c
@@ -1,13 +1,30 @@
-#include "iqrf/template.h"
+#include "iqrf/template-basic.h"
 
 void APPLICATION() {
 	enableSPI();
-
-	while (TRUE) {
-		if (RFRXpacket()) {
-			pulseLEDR();
-			copyBufferRF2COM();
-			startSPI(DLEN);
+	setRFmode(_WPE | _RX_STD | _TX_STD);
+	toutRF = 1;
+   
+	while(1) {
+		if(checkRF(0)) {
+			if(RFRXpacket()) {
+				pulseLEDR();
+				copyBufferRF2COM();
+				startSPI(DLEN);
+			}
+		}
+		if (getStatusSPI()) {
+			continue;
+		}
+		if (_SPIRX) {
+			if (_SPICRCok) {
+ 				DLEN = SPIpacketLength;
+				copyBufferCOM2RF();
+				PIN = 0;
+				RFTXpacket();
+				pulseLEDG();
+			}
+			startSPI(0);
 		}
 	}
 }
